a7e5254767bee14fef0f627dc1c017a24fcff4a8
marti <marta21st@gmail.com>
2019-07-04 11:51:46 -0400

    Adding file

---
+++ week1/Day2/day2.js
@@ -0,0 +1,32 @@
+// var weatherTemepreture = 40
+
+// console.log (weatherTemepreture)
+
+// if (weatherTemepreture < 10) {
+//     console.log ("it's cold")
+//  }
+// else if (weatherTemepreture > 20) {
+//     console.log ("it's warm")
+//  }
+// else {
+//     console.log ("it's perfect")
+//  }
+
+
+
+function weatherTemepreture(weather){
+
+//console.log (weatherTemepreture)
+
+if (weather < 10) {
+    return ("it's cold")
+ }
+else if (weather > 20) {
+    return ("it's warm")
+ }
+else {
+    return ("it's perfect")
+ }
+}
+  
+  console.log(weatherTemepreture(40))
---
+++ week1/Day4/day4.js
@@ -0,0 +1,14 @@
+var book ={
+  title: "Invisible Man",
+  author: "Ralph Ellison",
+  published: "April 14, 1952"
+}
+  console.log(book)
+  console.log(book.title)
+
+var food = ["Pasta", "Pizza", "Burger"]
+console.log (food)
+
+for (i=0; i<3; i++){
+  console.log (food[i])
+}
